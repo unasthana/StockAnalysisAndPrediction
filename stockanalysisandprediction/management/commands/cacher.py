0a0d1dd30a8a7a364de5388160d244de382502af
VivekBits2210 <viveknayak2210@gmail.com>
2023-12-11 06:24:45 -0500

    Updated correlations

--- stockanalysisandprediction/management/commands/cacher.py
+++ stockanalysisandprediction/management/commands/cacher.py
@@ -38,11 +38,11 @@ class Command(BaseCommand):
             "6_months",
             "1_year",
         ]
-        cache_clustering(time_list)
+        # cache_clustering(time_list)
 
         def cache_rankings(analytic_list, time_list, ma_analytic_list, ma_window_list):
-            base_url = lambda x: "http://127.0.0.1:8000/api/getRankings/{x}"
-            timeout = 300
+            base_url = lambda x: f"http://127.0.0.1:8000/api/getRankings/{x}/"
+            timeout = 1000
 
             for analytic in analytic_list:
                 for time in time_list:
@@ -63,14 +63,46 @@ class Command(BaseCommand):
                                     print(f"Response for {analytic}: {response.json()}")
                                 else:
                                     print(
-                                        f"Error for {analytic}: {response.status_code}"
+                                        f"Error for {analytic} {time} {ma_analytic} {ma_window}: {response.status_code}"
                                     )
                             except requests.exceptions.RequestException as e:
                                 print(f"Request failed for {analytic}: {e}")
 
                             sleep(1)
 
-        analytic_list = ["daily_returns", "daily_price_change", "daily_price_gap"]
-        ma_analytic_list = ["daily_returns", "daily_price_change", "daily_price_gap"]
-        ma_window_list = ["3_day", "5_day", "10_day", "30_day", "60_day"]
-        # cache_rankings(analytic_list, time_list, ma_analytic_list, ma_window_list)
+        analytic_list = ["daily_returns", "daily_price_change", "daily_price_gap", "yearly_performance", "daily_price_range"]
+        ma_analytic_list = ['NA', 'daily_returns', 'daily_price_change', 'daily_price_gap', "daily_price_gap", "yearly_performance", "daily_price_range"]
+        ma_window_list = ['NA', '3_day', '5_day', '10_day', '30_day', '60_day']
+        cache_rankings(analytic_list, time_list, ma_analytic_list, ma_window_list)
+
+        def cache_correlations(analytic_list, time_list, ma_analytic_list, ma_window_list):
+            base_url = lambda x, y: f"http://127.0.0.1:8000/api/getCorrelationAnalytics/{x}/{y}/"
+            timeout = 1000
+
+            for analytic in analytic_list:
+                for time in time_list:
+                    for ma_analytic in ma_analytic_list:
+                        for ma_window in ma_window_list:
+                            params = {
+                                "analytic": analytic,
+                                "time": time,
+                                "ma_analytic": ma_analytic,
+                                "ma_window": ma_window,
+                            }
+
+                            try:
+                                response = requests.get(
+                                    base_url(analytic), params=params, timeout=timeout
+                                )
+                                if response.status_code == 200:
+                                    print(f"Response for {analytic}: {response.json()}")
+                                else:
+                                    print(
+                                        f"Error for {analytic} {time} {ma_analytic} {ma_window}: {response.status_code}"
+                                    )
+                            except requests.exceptions.RequestException as e:
+                                print(f"Request failed for {analytic}: {e}")
+
+                            sleep(1)
+
+        cache_correlations(analytic_list, time_list, ma_analytic_list, ma_window_list)
